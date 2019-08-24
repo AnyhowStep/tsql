d7deaf1a76addf74f021eaa31dbf9a2c7756605d
anyhowstep <anyhowstep@hotmail.com>
2019-08-24 04:13:50 -0400

    Added more tests

---
+++ test/compile-time/input/from-clause/cross-join/can-reference-same-query-joins-if-lateral.ts
@@ -0,0 +1,40 @@
+import * as tm from "type-mapping/fluent";
+import * as tsql from "../../../../../dist";
+
+const myTable = tsql.table("myTable")
+    .addColumns({
+        myTableId : tm.mysql.bigIntSigned(),
+    })
+
+const myTable2 = tsql.table("myTable2")
+    .addColumns({
+        myTable2Id : tm.mysql.bigIntSigned(),
+    })
+
+declare const otherTable : tsql.AliasedTable<{
+    isLateral : true;
+    alias : "otherTable";
+    columns : {
+        otherTableId : tsql.IColumn<{
+            tableAlias : "otherTable",
+            columnAlias : "otherTableId",
+            mapper : tm.SafeMapper<bigint>,
+        }>
+    };
+    usedRef : tsql.IUsedRef<{
+        myTable2 : {
+            myTable2Id : bigint,
+        }
+    }>;
+}>;
+
+export const fromClause = tsql.FromClauseUtil.crossJoin(
+    tsql.FromClauseUtil.crossJoin(
+        tsql.FromClauseUtil.from(
+            tsql.FromClauseUtil.newInstance(),
+            myTable
+        ),
+        myTable2
+    ),
+    otherTable
+);
