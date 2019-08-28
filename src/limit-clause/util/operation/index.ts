8d3aa0883563c4d991f7fb38a1eeceb7cd67b723
anyhowstep <anyhowstep@hotmail.com>
2019-08-28 04:24:12 -0400

    Added exports

--- src/limit-clause/util/operation/index.ts
+++ src/limit-clause/util/operation/index.ts
@@ -1,2 +1,6 @@
+export * from "./limit-bigint";
+export * from "./limit-number";
 export * from "./limit";
+export * from "./offset-bigint";
+export * from "./offset-number";
 export * from "./offset";
