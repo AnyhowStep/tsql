333fd59fda91d57d69630251f61c297a14272ec1
anyhowstep <anyhowstep@hotmail.com>
2019-09-10 14:07:36 -0400

    Added another chainable-flatten test for zero and one operand

---
+++ test/run-time/input/expr-library/number-arithmetic/add/flatten-2.ts
@@ -0,0 +1,37 @@
+import * as tape from "tape";
+import * as tm from "type-mapping";
+import * as tsql from "../../../../../../dist";
+import {sqliteSqlfier} from "../../../../../sqlite-sqlfier";
+
+tape(__filename, t => {
+    const myTable = tsql.table("myTable")
+        .addColumns({
+            myColumn : tm.mysql.double(),
+        });
+
+    const expr = tsql.add(
+        myTable.columns.myColumn,
+        0,
+        tsql.add(
+            myTable.columns.myColumn,
+            0,
+            myTable.columns.myColumn,
+            tsql.add(
+                myTable.columns.myColumn,
+                0,
+                tsql.add(myTable.columns.myColumn),
+                myTable.columns.myColumn,
+                tsql.add()
+            ),
+            tsql.add(myTable.columns.myColumn),
+            tsql.add()
+        ),
+        myTable.columns.myColumn
+    );
+    t.deepEqual(
+        tsql.AstUtil.toSql(expr.ast, sqliteSqlfier),
+        `("myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn" + "myTable"."myColumn")`
+    );
+
+    t.end();
+});
