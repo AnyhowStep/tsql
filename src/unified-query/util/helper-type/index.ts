22eb18672ad998710d0e50c7e6a0873ab200fe76
anyhowstep <anyhowstep@hotmail.com>
2019-09-12 17:24:48 -0400

    Renamed union-clause to compoun-query-clause

--- src/unified-query/util/helper-type/index.ts
+++ src/unified-query/util/helper-type/index.ts
@@ -1,6 +1,6 @@
 export * from "./after-from-clause";
 export * from "./after-select-clause";
 export * from "./before-from-clause";
-export * from "./before-union-clause";
+export * from "./before-compound-query-clause";
 export * from "./correlated";
 export * from "./non-correlated";
