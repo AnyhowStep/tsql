95767eb599ac25a398ea39fb5bbbcb2e153a9eb7
anyhowstep <anyhowstep@hotmail.com>
2019-09-16 02:58:53 -0400

    Refactored indirectEval

--- test/run-time/input/sql-web-worker/worker-impl.sql.ts
+++ test/run-time/input/sql-web-worker/worker-impl.sql.ts
@@ -78,45 +78,48 @@ export function initWorker (
             }
             case SqliteAction.CREATE_FUNCTION: {
                 const db = await getOrCreateDb();
+                /**
+                 * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comma_Operator
+                 *
+                 * https://github.com/webpack/webpack/issues/9615#issuecomment-524167958
+                 *
+                 * According to https://github.com/fatcerberus,
+                 *
+                 * > Per spec the JS engine looks specifclaly for `eval(...` to trigger direct eval (inherits containing scope)
+                 * >
+                 * > `(0, eval)` bypasses it
+                 * >
+                 * > Indirect eval ensures you only inherit the global scope
+                 * >
+                 * > In case it wasn't clear the `0` can be anything you want
+                 * >
+                 * > But `0` is most concise
+                 *
+                 * ```js
+                 *  function bar () {
+                 *      const x = 4;
+                 *      eval("console.log(x)"); //prints 4
+                 *      (0, eval)("console.log(x)"); //Uncaught ReferenceError: x is not defined
+                 *  }
+                 *  bar();
+                 * ```
+                 *
+                 * ```js
+                 *  function bar () {
+                 *      const x = 4;
+                 *      eval("console.log(x)"); //prints 4
+                 *      const indirectEval = eval;
+                 *      indirectEval("console.log(x)"); //Uncaught ReferenceError: x is not defined
+                 *  }
+                 *  bar();
+                 * ```
+                 *
+                 * https://tc39.es/ecma262/#sec-function-calls-runtime-semantics-evaluation
+                 */
+                const indirectEval = eval;
                 db.create_function(
                     data.functionName,
-                    /**
-                     * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Comma_Operator
-                     *
-                     * https://github.com/webpack/webpack/issues/9615#issuecomment-524167958
-                     *
-                     * According to https://github.com/fatcerberus,
-                     *
-                     * > Per spec the JS engine looks specifclaly for `eval(...` to trigger direct eval (inherits containing scope)
-                     * >
-                     * > `(0, eval)` bypasses it
-                     * >
-                     * > Indirect eval ensures you only inherit the global scope
-                     * >
-                     * > In case it wasn't clear the `0` can be anything you want
-                     * >
-                     * > But `0` is most concise
-                     *
-                     * ```js
-                     *  function bar () {
-                     *      const x = 4
-                     *      eval("console.log(x)") //prints 4
-                     *      (0, eval)("console.log(x)") //Uncaught TypeError: eval(...) is not a function
-                     *  }
-                     *  bar()
-                     * ```
-                     *
-                     * ```js
-                     *  function bar () {
-                     *      const x = 4
-                     *      eval("console.log(x)") //prints 4
-                     *      const indirectEval = eval;
-                     *      indirectEval("console.log(x)") //Uncaught TypeError: eval(...) is not a function
-                     *  }
-                     *  bar()
-                     * ```
-                     */
-                    (0, eval)("(" + data.impl + ")")
+                    indirectEval("(" + data.impl + ")")
                 );
                 postMessage({
                     id : data.id,
