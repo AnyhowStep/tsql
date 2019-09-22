f9b73b2820d0f5f5e83db5b4393694d8a75a7342
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-09-22 07:32:28 -0400

    Added long chain test for coalesce() for Microsoft/TypeScript#33541

---
+++ test/compile-time/expected-output/expr-library/comparison/coalesce/long-chain.d.ts
@@ -0,0 +1,36 @@
+import * as tsql from "../../../../../../dist";
+export declare const expr0: tsql.ExprImpl<number, tsql.IUsedRef<{
+    readonly myTable: {
+        readonly someColumnC: number;
+        readonly someColumnA: number;
+        readonly someColumnB: number;
+    };
+}>>;
+export declare const expr1: tsql.ExprImpl<number, tsql.IUsedRef<{
+    readonly myTable: {
+        readonly someColumnC: number;
+        readonly someColumnA: number;
+        readonly someColumnB: number;
+    };
+}>>;
+export declare const expr2: tsql.ExprImpl<number, tsql.IUsedRef<{
+    readonly myTable: {
+        readonly someColumnC: number;
+        readonly someColumnA: number;
+        readonly someColumnB: number;
+    };
+}>>;
+export declare const expr3: tsql.ExprImpl<number, tsql.IUsedRef<{
+    readonly myTable: {
+        readonly someColumnC: number;
+        readonly someColumnA: number;
+        readonly someColumnB: number;
+    };
+}>>;
+export declare const expr4: tsql.ExprImpl<number, tsql.IUsedRef<{
+    readonly myTable: {
+        readonly someColumnC: number;
+        readonly someColumnA: number;
+        readonly someColumnB: number;
+    };
+}>>;
