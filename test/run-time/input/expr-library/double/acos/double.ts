c413c1152ae7b65eb2e2a18ee985d6dbb8b31295
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-09-22 07:24:49 -0400

    Repro for Microsoft/TypeScript#33541

--- test/run-time/input/expr-library/double/acos/double.ts
+++ test/run-time/input/expr-library/double/acos/double.ts
@@ -9,9 +9,24 @@ tape(__filename, t => {
             myColumn : tm.mysql.double(),
         });
 
-    const expr = tsql.double.acos(tsql.coalesce(tsql.double.acos(
-        myTable.columns.myColumn
-    ), 0));
+    //This is OK, after assigning to a temporary variable
+    const tmp = tsql.coalesce(
+        tsql.double.acos(myTable.columns.myColumn),
+        0
+    );
+    const expr2 = tsql.double.acos(
+        tmp
+    );
+    expr2;
+
+    //This gives the `Type instantiation is excessively deep and possibly infinite.`
+    //error.
+    const expr = tsql.double.acos(
+        tsql.coalesce(
+            tsql.double.acos(myTable.columns.myColumn),
+            0
+        )
+    );
     t.deepEqual(
         tsql.AstUtil.toSql(expr.ast, sqliteSqlfier),
         `ACOS(COALESCE(ACOS("myTable"."myColumn"), 0e0))`
