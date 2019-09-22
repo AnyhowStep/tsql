65c87ccfbe4db3b3b77d3c7ee59e4397bbc9afc0
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-09-22 04:00:53 -0400

    Updated compile-time-test

--- test/compile-time/expected-output/unified-query/as/can-be-used-as-expr-2.d.ts
+++ test/compile-time/expected-output/unified-query/as/can-be-used-as-expr-2.d.ts
@@ -1,7 +1,7 @@
 import * as tm from "type-mapping/fluent";
 import * as tsql from "../../../../../dist";
 export declare const aliased: tsql.ExprImpl<tm.Mapper<unknown, boolean>, tsql.IUsedRef<{
-    somethingElse: {
+    readonly somethingElse: {
         boop: bigint;
     };
 }>>;
