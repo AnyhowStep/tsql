d06e8b556cd98d0f98af37c87d6c34d7237aa697
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-12-06 11:52:13 -0500

    Added tests for rowMapper()

---
+++ test/run-time/input/design-pattern-table-per-type/fetch-one/no-parents-row-mapper.ts
@@ -0,0 +1,46 @@
+import * as tape from "tape";
+import * as tsql from "../../../../../dist";
+import {Pool} from "../../sql-web-worker/promise.sql";
+import {SqliteWorker} from "../../sql-web-worker/worker.sql";
+import {appKey, createAppKeyTableSql, appKeyTpt} from "../app-key-example";
+
+tape(__filename, async (t) => {
+    const pool = new Pool(new SqliteWorker());
+
+    const fetchOneResult = await pool.acquire(async (connection) => {
+        await connection.exec(createAppKeyTableSql);
+
+        await appKey.insertOne(
+            connection,
+            {
+                appId : BigInt(1),
+                appKeyTypeId : BigInt(1) as 1n,
+                key : "server",
+                createdAt : new Date(1),
+                disabledAt : new Date(2),
+            }
+        );
+
+        return appKeyTpt.fetchOne(
+            connection,
+            (columns) => tsql.eq(
+                columns.appKeyId,
+                BigInt(1)
+            )
+        );
+    });
+
+    t.deepEqual(
+        tsql.TablePerTypeUtil.rowMapper(appKeyTpt)("", fetchOneResult),
+        {
+            appKeyId: BigInt(1),
+            appKeyTypeId: BigInt(1),
+            appId: BigInt(1),
+            key: "server",
+            createdAt: new Date(1),
+            disabledAt: new Date(2),
+        }
+    );
+
+    t.end();
+});
