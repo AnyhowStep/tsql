39e1327034db243ca22d8ea25731ed1d176f54df
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-12-07 03:54:22 -0500

    Added more tests

---
+++ test/run-time/input/async-queue/lock/sync-deallocate-error-no-enqueue.ts
@@ -0,0 +1,38 @@
+import * as tape from "tape";
+import * as tsql from "../../../../../dist";
+
+tape(__filename, async (t) => {
+    const queue = new tsql.AsyncQueue(() => {
+        return {
+            item : undefined,
+            deallocate : () => {
+                throw new Error("deallocate error");
+            }
+        };
+    });
+    await queue.lock(() => {
+        return Promise.resolve(1);
+    }).then((result) => {
+        t.deepEqual(result, 1);
+    }).catch((err) => {
+        t.fail(String(err));
+    });
+
+    await queue.lock(() => {
+        return Promise.resolve(1);
+    }).then((result) => {
+        t.deepEqual(result, 1);
+    }).catch((err) => {
+        t.fail(String(err));
+    });
+
+    await queue.enqueue(() => {
+        return Promise.resolve(1);
+    }).then((result) => {
+        t.deepEqual(result, 1);
+    }).catch((err) => {
+        t.fail(String(err));
+    });
+
+    t.end();
+});
---
+++ test/run-time/input/execution/transaction/leaky-transaction.ts
@@ -0,0 +1,57 @@
+import * as tm from "type-mapping";
+import * as tape from "tape";
+import * as tsql from "../../../../../dist";
+import {Pool} from "../../sql-web-worker/promise.sql";
+import {SqliteWorker} from "../../sql-web-worker/worker.sql";
+
+tape(__filename, async (t) => {
+    const pool = new Pool(new SqliteWorker());
+
+    const src = tsql.table("src")
+        .addColumns({
+            testId : tm.mysql.bigIntUnsigned(),
+            testVal : tm.mysql.bigIntUnsigned(),
+        })
+        .setPrimaryKey(columns => [columns.testId]);
+
+    await pool.acquire(async (connection) => {
+        await connection.exec(`
+            CREATE TABLE src (
+                testId INT PRIMARY KEY,
+                testVal INT
+            );
+
+            INSERT INTO src(testId, testVal) VALUES
+                (1,100),
+                (2,200),
+                (3,300);
+        `);
+
+        const leaky = await connection.transaction(async (leaky) => {
+            return leaky;
+        });
+
+        await src.fetchOneByPrimaryKey(
+            leaky,
+            {
+                testId : BigInt(1),
+            }
+        ).then(() => {
+            t.fail("Should not be able to use leaked locked connection");
+        }).catch((err) => {
+            t.true(err instanceof tsql.AsyncQueueStoppingError);
+        });
+
+        await leaky
+            .lock(async () => {
+                t.fail("Should not have lock");
+            })
+            .then(() => {
+                t.fail("Should not be able to use leaked locked connection");
+            }).catch((err) => {
+                t.true(err instanceof tsql.AsyncQueueStoppingError);
+            });
+    });
+
+    t.end();
+});
