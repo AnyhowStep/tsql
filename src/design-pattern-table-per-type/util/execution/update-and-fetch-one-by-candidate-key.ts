eb9698ae86d902aa2bb1ae84b0e2fac9cf1d9fbe
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-12-13 17:28:44 -0500

    Moved to transaction

--- src/design-pattern-table-per-type/util/execution/update-and-fetch-one-by-candidate-key.ts
+++ src/design-pattern-table-per-type/util/execution/update-and-fetch-one-by-candidate-key.ts
@@ -148,183 +148,186 @@ export async function updateAndFetchOneByCandidateKey<
      * or even columns at all.
      */
     const rawAssignmentMap = assignmentMapDelegate(columns);
-    let processedAssignmentMap : Record<string, unknown>|undefined = undefined;
-    if (Object.keys(rawAssignmentMap).length == 0) {
-        processedAssignmentMap = {};
-    } else {
-        const query = from(tpt)
-            .where(() => ExprLib.eqCandidateKey(
-                tpt.childTable,
-                candidateKey
-            ) as any)
-            .select(() => Object
-                .keys(rawAssignmentMap)
-                .filter(columnAlias => isMutableColumnAlias(tpt, columnAlias))
-                .map(columnAlias => {
-                    const customExpr = rawAssignmentMap[columnAlias as keyof typeof rawAssignmentMap] as any;
-                    if (BuiltInExprUtil.isAnyNonValueExpr(customExpr)) {
-                        /**
-                         * We have a non-value expression
-                         */
-                        return expr(
-                            {
-                                mapper : columnMapper(tpt, columnAlias),
-                                usedRef : BuiltInExprUtil.usedRef(customExpr),
-                            },
-                            BuiltInExprUtil.buildAst(customExpr)
-                        ).as(columnAlias);
-                    } else {
-                        /**
-                         * We have a value expression
-                         */
-                        return ExprUtil.fromRawExprNoUsedRefInput(
-                            columnMapper(tpt, columnAlias),
-                            customExpr
-                        ).as(columnAlias);
-                    }
-                }) as any
-            );
-        /**
-         * Should only contain value expressions now.
-         */
-        processedAssignmentMap = await ExecutionUtil.fetchOne(
-            query as any,
-            connection
-        ) as Record<string, unknown>;
-    }
 
-    const cleanedAssignmentMap = processedAssignmentMap;
-
-    /**
-     * @todo If `result` contains any primaryKey values,
-     * then we will need to fetch the **current** primaryKey values,
-     * before any `UPDATE` statements are executed.
-     *
-     * This function breaks if we try to update values
-     * of columns that are foreign keys.
-     *
-     * I do not want to disable foreign key checks.
-     */
-    const updateAndFetchChildResult = await ExecutionUtil.updateAndFetchOneByCandidateKey(
-        tpt.childTable,
-        connection,
-        candidateKey,
-        () => pickOwnEnumerable(
-            cleanedAssignmentMap,
-            tpt.childTable.mutableColumns
-        )
-    );
-    const updateOneResults : UpdateAndFetchOneResult<any>["updateOneResults"] = [
-        {
-            ...updateAndFetchChildResult,
-            table : tpt.childTable,
-        },
-    ];
-    let updatedRowCount : bigint = updateAndFetchChildResult.updatedRowCount;
-    let warningCount : bigint = updateAndFetchChildResult.warningCount;
-
-    const result : Record<string, unknown> = updateAndFetchChildResult.row;
-
-    /**
-     * We use `.reverse()` here to `UPDATE` the parents
-     * as we go up the inheritance hierarchy.
-     */
-    for(const parentTable of [...tpt.parentTables].reverse()) {
-        const updateAndFetchParentResult = await ExecutionUtil.updateAndFetchOneByPrimaryKey(
-            parentTable,
-            connection,
+    return connection.transactionIfNotInOne(async (connection) : Promise<UpdateAndFetchOneReturnType<TptT, AssignmentMapT>> => {
+        let processedAssignmentMap : Record<string, unknown>|undefined = undefined;
+        if (Object.keys(rawAssignmentMap).length == 0) {
+            processedAssignmentMap = {};
+        } else {
+            const query = from(tpt)
+                .where(() => ExprLib.eqCandidateKey(
+                    tpt.childTable,
+                    candidateKey
+                ) as any)
+                .select(() => Object
+                    .keys(rawAssignmentMap)
+                    .filter(columnAlias => isMutableColumnAlias(tpt, columnAlias))
+                    .map(columnAlias => {
+                        const customExpr = rawAssignmentMap[columnAlias as keyof typeof rawAssignmentMap] as any;
+                        if (BuiltInExprUtil.isAnyNonValueExpr(customExpr)) {
+                            /**
+                             * We have a non-value expression
+                             */
+                            return expr(
+                                {
+                                    mapper : columnMapper(tpt, columnAlias),
+                                    usedRef : BuiltInExprUtil.usedRef(customExpr),
+                                },
+                                BuiltInExprUtil.buildAst(customExpr)
+                            ).as(columnAlias);
+                        } else {
+                            /**
+                             * We have a value expression
+                             */
+                            return ExprUtil.fromRawExprNoUsedRefInput(
+                                columnMapper(tpt, columnAlias),
+                                customExpr
+                            ).as(columnAlias);
+                        }
+                    }) as any
+                );
             /**
-             * The `result` should contain the primary key values we are interested in
+             * Should only contain value expressions now.
              */
-            result,
+            processedAssignmentMap = await ExecutionUtil.fetchOne(
+                query as any,
+                connection
+            ) as Record<string, unknown>;
+        }
+
+        const cleanedAssignmentMap = processedAssignmentMap;
+
+        /**
+         * @todo If `result` contains any primaryKey values,
+         * then we will need to fetch the **current** primaryKey values,
+         * before any `UPDATE` statements are executed.
+         *
+         * This function breaks if we try to update values
+         * of columns that are foreign keys.
+         *
+         * I do not want to disable foreign key checks.
+         */
+        const updateAndFetchChildResult = await ExecutionUtil.updateAndFetchOneByCandidateKey(
+            tpt.childTable,
+            connection,
+            candidateKey,
             () => pickOwnEnumerable(
                 cleanedAssignmentMap,
-                parentTable.mutableColumns
+                tpt.childTable.mutableColumns
             )
         );
-        updateOneResults.push({
-            ...updateAndFetchParentResult,
-            table : parentTable,
-        });
-        updatedRowCount = tm.BigIntUtil.add(
-            updatedRowCount,
-            updateAndFetchParentResult.updatedRowCount
-        );
-        warningCount = tm.BigIntUtil.add(
-            warningCount,
-            updateAndFetchParentResult.warningCount
-        );
-        const row = updateAndFetchParentResult.row;
+        const updateOneResults : UpdateAndFetchOneResult<any>["updateOneResults"] = [
+            {
+                ...updateAndFetchChildResult,
+                table : tpt.childTable,
+            },
+        ];
+        let updatedRowCount : bigint = updateAndFetchChildResult.updatedRowCount;
+        let warningCount : bigint = updateAndFetchChildResult.warningCount;
 
-        for (const columnAlias of Object.keys(row)) {
-            /**
-             * This is guaranteed to be a value expression.
-             */
-            const newValue = row[columnAlias];
+        const result : Record<string, unknown> = updateAndFetchChildResult.row;
 
-            if (Object.prototype.hasOwnProperty.call(result, columnAlias)) {
+        /**
+         * We use `.reverse()` here to `UPDATE` the parents
+         * as we go up the inheritance hierarchy.
+         */
+        for(const parentTable of [...tpt.parentTables].reverse()) {
+            const updateAndFetchParentResult = await ExecutionUtil.updateAndFetchOneByPrimaryKey(
+                parentTable,
+                connection,
                 /**
-                 * This `curValue` could be a non-value expression.
-                 * We only want value expressions.
+                 * The `result` should contain the primary key values we are interested in
                  */
-                const curValue = result[columnAlias];
+                result,
+                () => pickOwnEnumerable(
+                    cleanedAssignmentMap,
+                    parentTable.mutableColumns
+                )
+            );
+            updateOneResults.push({
+                ...updateAndFetchParentResult,
+                table : parentTable,
+            });
+            updatedRowCount = tm.BigIntUtil.add(
+                updatedRowCount,
+                updateAndFetchParentResult.updatedRowCount
+            );
+            warningCount = tm.BigIntUtil.add(
+                warningCount,
+                updateAndFetchParentResult.warningCount
+            );
+            const row = updateAndFetchParentResult.row;
 
-                if (BuiltInExprUtil.isAnyNonValueExpr(curValue)) {
+            for (const columnAlias of Object.keys(row)) {
+                /**
+                 * This is guaranteed to be a value expression.
+                 */
+                const newValue = row[columnAlias];
+
+                if (Object.prototype.hasOwnProperty.call(result, columnAlias)) {
+                    /**
+                     * This `curValue` could be a non-value expression.
+                     * We only want value expressions.
+                     */
+                    const curValue = result[columnAlias];
+
+                    if (BuiltInExprUtil.isAnyNonValueExpr(curValue)) {
+                        /**
+                         * Add this new value to the `result`
+                         * so we can use it to update rows of tables
+                         * further down the inheritance hierarchy.
+                         */
+                        result[columnAlias] = newValue;
+                        continue;
+                    }
+
+                    if (curValue === newValue) {
+                        /**
+                         * They are equal, do nothing.
+                         */
+                        continue;
+                    }
+                    /**
+                     * We need some custom equality checking logic
+                     */
+                    if (!DataTypeUtil.isNullSafeEqual(
+                        parentTable.columns[columnAlias],
+                        /**
+                         * This may throw
+                         */
+                        parentTable.columns[columnAlias].mapper(
+                            `${parentTable.alias}.${columnAlias}`,
+                            curValue
+                        ),
+                        newValue
+                    )) {
+                        /**
+                         * @todo Custom `Error` type
+                         */
+                        throw new Error(`All columns with the same name in an inheritance hierarchy must have the same value; mismatch found for ${parentTable.alias}.${columnAlias}`);
+                    }
+                } else {
                     /**
                      * Add this new value to the `result`
                      * so we can use it to update rows of tables
                      * further down the inheritance hierarchy.
                      */
                     result[columnAlias] = newValue;
-                    continue;
                 }
-
-                if (curValue === newValue) {
-                    /**
-                     * They are equal, do nothing.
-                     */
-                    continue;
-                }
-                /**
-                 * We need some custom equality checking logic
-                 */
-                if (!DataTypeUtil.isNullSafeEqual(
-                    parentTable.columns[columnAlias],
-                    /**
-                     * This may throw
-                     */
-                    parentTable.columns[columnAlias].mapper(
-                        `${parentTable.alias}.${columnAlias}`,
-                        curValue
-                    ),
-                    newValue
-                )) {
-                    /**
-                     * @todo Custom `Error` type
-                     */
-                    throw new Error(`All columns with the same name in an inheritance hierarchy must have the same value; mismatch found for ${parentTable.alias}.${columnAlias}`);
-                }
-            } else {
-                /**
-                 * Add this new value to the `result`
-                 * so we can use it to update rows of tables
-                 * further down the inheritance hierarchy.
-                 */
-                result[columnAlias] = newValue;
             }
         }
-    }
 
-    return {
-        updateOneResults,
-        /**
-         * +1 for the `childTable`.
-         */
-        foundRowCount : tm.BigInt(tpt.parentTables.length + 1),
-        updatedRowCount,
+        return {
+            updateOneResults,
+            /**
+             * +1 for the `childTable`.
+             */
+            foundRowCount : tm.BigInt(tpt.parentTables.length + 1),
+            updatedRowCount,
 
-        warningCount,
+            warningCount,
 
-        row : result as UpdatedAndFetchedRow<TptT, AssignmentMapT>,
-    };
+            row : result as UpdatedAndFetchedRow<TptT, AssignmentMapT>,
+        };
+    });
 }
