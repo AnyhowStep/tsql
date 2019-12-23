449a4d16d0a8c593937bd9264b7b71a3811f10cd
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2019-12-23 14:43:08 -0500

    Added more tests

---
+++ test/run-time/input/pool/on-update/transaction-commit.ts
@@ -0,0 +1,132 @@
+import * as tm from "type-mapping";
+import * as tape from "tape";
+import * as tsql from "../../../../../dist";
+import {Pool} from "../../sql-web-worker/promise.sql";
+import {SqliteWorker} from "../../sql-web-worker/worker.sql";
+
+tape(__filename, async (t) => {
+    const pool = new Pool(new SqliteWorker());
+
+    const test = tsql.table("test")
+        .addColumns({
+            testId : tm.mysql.bigIntUnsigned(),
+            testVal : tm.mysql.bigIntUnsigned(),
+        })
+        .setPrimaryKey(columns => [columns.testId])
+        .addAllMutable();
+
+    let eventHandled = false;
+    let onCommitInvoked = false;
+    let onRollbackInvoked = false;
+    pool.onUpdate.addHandler((event) => {
+        if (!event.isFor(test)) {
+            return;
+        }
+        event.addOnCommitListener(() => {
+            onCommitInvoked = true;
+        });
+        event.addOnRollbackListener(() => {
+            onRollbackInvoked = true;
+        });
+        eventHandled = true;
+        t.deepEqual(
+            event.updateResult.foundRowCount,
+            BigInt(1)
+        );
+    });
+
+    const insertResult = await pool.acquire(async (connection) => {
+        await connection.exec(`
+            CREATE TABLE test (
+                testId INT PRIMARY KEY,
+                testVal INT
+            );
+            INSERT INTO
+                test(testId, testVal)
+            VALUES
+                (1, 100),
+                (2, 200),
+                (3, 300);
+        `);
+
+        t.deepEqual(eventHandled, false);
+        t.deepEqual(onCommitInvoked, false);
+        t.deepEqual(onRollbackInvoked, false);
+
+        const result = await connection.transaction(async (connection) => {
+
+            t.deepEqual(eventHandled, false);
+            t.deepEqual(onCommitInvoked, false);
+            t.deepEqual(onRollbackInvoked, false);
+
+            const result = await test.insertOne(
+                connection,
+                {
+                    testId : BigInt(4),
+                    testVal : BigInt(400),
+                }
+            );
+
+            t.deepEqual(eventHandled, false);
+            t.deepEqual(onCommitInvoked, false);
+            t.deepEqual(onRollbackInvoked, false);
+
+            await test.updateOneByPrimaryKey(
+                connection,
+                {
+                    testId : BigInt(4),
+                },
+                () => {
+                    return {
+                        testVal : BigInt(444),
+                    };
+                }
+            );
+
+            t.deepEqual(eventHandled, true);
+            t.deepEqual(onCommitInvoked, false);
+            t.deepEqual(onRollbackInvoked, false);
+
+            return result;
+        });
+
+        t.deepEqual(eventHandled, true);
+        t.deepEqual(onCommitInvoked, true);
+        t.deepEqual(onRollbackInvoked, false);
+
+        return result;
+    });
+
+    t.deepEqual(eventHandled, true);
+    t.deepEqual(onCommitInvoked, true);
+    t.deepEqual(onRollbackInvoked, false);
+
+    t.deepEqual(
+        insertResult.insertedRowCount,
+        BigInt(1)
+    );
+    t.deepEqual(
+        insertResult.autoIncrementId,
+        undefined
+    );
+    t.deepEqual(
+        insertResult.warningCount,
+        BigInt(0)
+    );
+
+    await pool
+        .acquire(async (connection) => {
+            return test.fetchOneByPrimaryKey(connection, { testId : BigInt(4) });
+        })
+        .then((row) => {
+            t.deepEqual(
+                row,
+                {
+                    testId : BigInt(4),
+                    testVal : BigInt(444),
+                }
+            );
+        });
+
+    t.end();
+});
