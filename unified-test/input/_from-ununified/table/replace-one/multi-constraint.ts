12f1ca72327f9ec9e39b760185775d3e2ad9dee4
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-01-20 16:47:35 -0500

    Unified test migration for table.replaceOne()

---
+++ unified-test/input/_from-ununified/table/replace-one/multi-constraint.ts
@@ -0,0 +1,119 @@
+import * as tm from "type-mapping";
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool, createTemporarySchema}) => {
+    tape(__filename, async (t) => {
+        const test = tsql.table("test")
+            .addColumns({
+                testId : tm.mysql.bigIntUnsigned(),
+                testVal : tm.mysql.bigIntUnsigned(),
+            })
+            .setPrimaryKey(columns => [columns.testId]);
+
+        const insertResult = await pool.acquire(async (connection) => {
+            await createTemporarySchema(
+                connection,
+                {
+                    tables : [
+                        {
+                            tableAlias : "test",
+                            columns : [
+                                {
+                                    columnAlias : "testId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "testVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                            primaryKey : {
+                                multiColumn : false,
+                                columnAlias : "testId",
+                                autoIncrement : false,
+                            },
+                            candidateKeys : [
+                                ["testVal"],
+                            ],
+                        }
+                    ]
+                }
+            );
+
+            await test
+                .enableExplicitAutoIncrementValue()
+                .insertMany(
+                    connection,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                        {
+                            testId : BigInt(4),
+                            testVal : BigInt(444),
+                        },
+                    ]
+                );
+
+            return test.replaceOne(
+                connection,
+                {
+                    testId : BigInt(5),
+                    testVal : BigInt(444),
+                }
+            );
+        });
+        t.deepEqual(
+            insertResult.insertedOrReplacedRowCount,
+            BigInt(1)
+        );
+        t.deepEqual(
+            insertResult.warningCount,
+            BigInt(0)
+        );
+
+        await pool
+            .acquire(async (connection) => {
+                return tsql.from(test).select(columns => [columns]).fetchAll(connection);
+            })
+            .then((rows) => {
+                t.deepEqual(
+                    rows,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                        {
+                            testId : BigInt(5),
+                            testVal : BigInt(444),
+                        },
+                    ]
+                );
+            });
+
+        t.end();
+    });
+};
