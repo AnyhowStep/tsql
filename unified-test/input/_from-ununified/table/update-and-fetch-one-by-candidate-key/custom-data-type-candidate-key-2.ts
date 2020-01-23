492da2da6d868bf45246b82c1d4c9c866e4e374a
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-01-23 00:34:04 -0500

    Unified test migration for table.updateAndFetchOneByCandidateKey()

---
+++ unified-test/input/_from-ununified/table/update-and-fetch-one-by-candidate-key/custom-data-type-candidate-key-2.ts
@@ -0,0 +1,123 @@
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+import {dtPoint} from "../../dt-point";
+
+export const test : Test = ({tape, pool, createTemporarySchema}) => {
+    tape(__filename, async (t) => {
+        const test = tsql.table("test")
+            .addColumns({
+                testId : dtPoint,
+                testVal : dtPoint,
+            })
+            .addCandidateKey(columns => [columns.testId])
+            .addAllMutable();
+
+        const updateAndFetchResult = await pool.acquire(async (connection) => {
+            await createTemporarySchema(
+                connection,
+                {
+                    tables : [
+                        {
+                            tableAlias : "test",
+                            columns : [
+                                {
+                                    columnAlias : "testId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.STRING,
+                                    },
+                                },
+                                {
+                                    columnAlias : "testVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.STRING,
+                                    },
+                                },
+                            ],
+                            primaryKey : {
+                                multiColumn : false,
+                                columnAlias : "testId",
+                                autoIncrement : false,
+                            },
+                        }
+                    ]
+                }
+            );
+
+            await test.insertAndFetch(
+                connection,
+                {
+                    testId : {
+                        x : 100,
+                        y : 200,
+                    },
+                    testVal : {
+                        x : 1,
+                        y : 2,
+                    },
+                }
+            );
+
+            return test
+                .whereEqCandidateKey({
+                    testId : {
+                        x : 100,
+                        y : 200,
+                    },
+                })
+                .updateAndFetchOne(
+                    connection,
+                    () => {
+                        return {
+                            testVal : {
+                                x : 111,
+                                y : 222,
+                            }
+                        };
+                    }
+                );
+        });
+        t.deepEqual(
+            updateAndFetchResult.row,
+            {
+                testId : {
+                    x : 100,
+                    y : 200,
+                },
+                testVal : {
+                    x : 111,
+                    y : 222,
+                },
+            }
+        );
+
+        await pool
+            .acquire(async (connection) => {
+                return test.fetchOneByCandidateKey(
+                    connection,
+                    {
+                        testId : {
+                            x : 100,
+                            y : 200,
+                        }
+                    }
+                );
+            })
+            .then((row) => {
+                t.deepEqual(
+                    row,
+                    {
+                        testId : {
+                            x : 100,
+                            y : 200,
+                        },
+                        testVal : {
+                            x : 111,
+                            y : 222,
+                        },
+                    }
+                );
+            });
+
+        t.end();
+    });
+};
---
+++ unified-test/input/_from-ununified/table/update-and-fetch-one-by-candidate-key/update-can-change-candidate-key-to-other-primitive-2.ts
@@ -0,0 +1,137 @@
+import * as tm from "type-mapping";
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool, createTemporarySchema}) => {
+    tape(__filename, async (t) => {
+        const dst = tsql.table("dst")
+            .addColumns({
+                testId : tm.mysql.bigIntUnsigned(),
+                testVal : tm.mysql.bigIntUnsigned().orNull(),
+            })
+            .setPrimaryKey(columns => [columns.testId])
+            .addMutable(columns => [
+                columns.testId,
+                columns.testVal,
+            ]);
+
+        const result = await pool.acquire(async (connection) => {
+            await createTemporarySchema(
+                connection,
+                {
+                    tables : [
+                        {
+                            tableAlias : "dst",
+                            columns : [
+                                {
+                                    columnAlias : "testId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "testVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                            primaryKey : {
+                                multiColumn : false,
+                                columnAlias : "testId",
+                                autoIncrement : false,
+                            },
+                        }
+                    ]
+                }
+            );
+
+            await dst
+                .enableExplicitAutoIncrementValue()
+                .insertMany(
+                    connection,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                    ]
+                );
+
+            return dst
+                .whereEqCandidateKey({
+                    testId : BigInt(1),
+                })
+                .updateAndFetchOne(
+                    connection,
+                    columns => {
+                        return {
+                            testId : BigInt(123456),
+                            testVal : tsql.integer.add(
+                                tsql.coalesce(columns.testVal, BigInt(777)),
+                                BigInt(50)
+                            ),
+                        };
+                    }
+                );
+        });
+        t.deepEqual(
+            result.foundRowCount,
+            BigInt(1)
+        );
+        t.deepEqual(
+            result.updatedRowCount,
+            BigInt(1)
+        );
+        t.deepEqual(
+            result.warningCount,
+            BigInt(0)
+        );
+        t.deepEqual(
+            result.row,
+            {
+                testId : BigInt(123456),
+                testVal : BigInt(150),
+            }
+        );
+
+        await pool
+            .acquire(async (connection) => {
+                return tsql.from(dst)
+                    .select(columns => [columns])
+                    .orderBy(columns => [
+                        columns.testId.asc(),
+                    ])
+                    .fetchAll(connection);
+            })
+            .then((rows) => {
+                t.deepEqual(
+                    rows,
+                    [
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                        {
+                            testId : BigInt(123456),
+                            testVal : BigInt(150),
+                        },
+                    ]
+                );
+            });
+
+        t.end();
+    });
+};
