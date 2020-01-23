9e2700835fcd4f42bfc00931db37889786eafc6e
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-01-23 17:35:46 -0500

    cleanup

--- unified-test/input/_from-ununified/table/update-zero-or-one-by-candidate-key/many.ts
+++ unified-test/input/_from-ununified/table/update-zero-or-one-by-candidate-key/many.ts
@@ -3,128 +3,131 @@ import {Test} from "../../../../test";
 import * as tsql from "../../../../../dist";
 
 export const test : Test = ({tape, pool, createTemporarySchema}) => {
-tape(__filename, async (t) => {
-    const dst = tsql.table("dst")
-        .addColumns({
-            testId : tm.mysql.bigIntUnsigned(),
-            testVal : tm.mysql.bigIntUnsigned(),
-        })
-        .setPrimaryKey(columns => [columns.testId])
-        .addMutable(columns => [
-            columns.testVal,
-        ]);
+    tape(__filename, async (t) => {
+        const dst = tsql.table("dst")
+            .addColumns({
+                testId : tm.mysql.bigIntUnsigned(),
+                testVal : tm.mysql.bigIntUnsigned(),
+            })
+            .setPrimaryKey(columns => [columns.testId])
+            .addMutable(columns => [
+                columns.testVal,
+            ]);
 
-    await pool.acquire(async (connection) => {
-        await createTemporarySchema(
-            connection,
-            {
-                tables : [
-                    {
-                        tableAlias : "dst",
-                        columns : [
-                            {
-                                columnAlias : "testId",
-                                dataType : {
-                                    typeHint : tsql.TypeHint.BIGINT_SIGNED,
-                                },
-                            },
-                            {
-                                columnAlias : "testVal",
-                                dataType : {
-                                    typeHint : tsql.TypeHint.BIGINT_SIGNED,
-                                },
-                            },
-                        ],
-                        /**
-                         * We don't actually have a primary key,
-                         * even though we say we do.
-                         *
-                         * We are lying to try and trick the library
-                         * into updating more than one row,
-                         * which should not happen.
-                         */
-                    }
-                ]
-            }
-        );
-
-        await dst
-            .enableExplicitAutoIncrementValue()
-            .insertMany(
+        await pool.acquire(async (connection) => {
+            await createTemporarySchema(
                 connection,
-                [
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(2),
-                        testVal : BigInt(200),
-                    },
-                    {
-                        testId : BigInt(3),
-                        testVal : BigInt(300),
-                    },
-                ]
+                {
+                    tables : [
+                        {
+                            tableAlias : "dst",
+                            columns : [
+                                {
+                                    columnAlias : "testId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "testVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                            /**
+                             * We don't actually have a primary key,
+                             * even though we say we do.
+                             *
+                             * We are lying to try and trick the library
+                             * into updating more than one row,
+                             * which should not happen.
+                             */
+                        }
+                    ]
+                }
             );
 
-        return dst.updateZeroOrOneByCandidateKey(
-            connection,
-            {
-                testId : BigInt(1),
-            },
-            columns => {
-                return {
-                    testVal : tsql.integer.add(
-                        columns.testVal,
-                        BigInt(50)
-                    ),
-                };
-            }
-        ).then(() => {
-            t.fail("Should not update anything");
-        }).catch((err) => {
-            t.true(err instanceof tsql.TooManyRowsFoundError);
+            await dst
+                .enableExplicitAutoIncrementValue()
+                .insertMany(
+                    connection,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                    ]
+                );
+
+            return dst
+                .whereEqCandidateKey({
+                    testId : BigInt(1),
+                })
+                .updateZeroOrOne(
+                    connection,
+                    columns => {
+                        return {
+                            testVal : tsql.integer.add(
+                                columns.testVal,
+                                BigInt(50)
+                            ),
+                        };
+                    }
+                )
+                .then(() => {
+                    t.fail("Should not update anything");
+                })
+                .catch((err) => {
+                    t.true(err instanceof tsql.TooManyRowsFoundError);
+                });
         });
+
+        await pool
+            .acquire(async (connection) => {
+                return tsql.from(dst)
+                    .select(columns => [columns])
+                    .orderBy(columns => [
+                        columns.testId.asc(),
+                    ])
+                    .fetchAll(connection);
+            })
+            .then((rows) => {
+                t.deepEqual(
+                    rows,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                    ]
+                );
+            });
+
+        t.end();
     });
-
-    await pool
-        .acquire(async (connection) => {
-            return tsql.from(dst)
-                .select(columns => [columns])
-                .orderBy(columns => [
-                    columns.testId.asc(),
-                ])
-                .fetchAll(connection);
-        })
-        .then((rows) => {
-            t.deepEqual(
-                rows,
-                [
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(2),
-                        testVal : BigInt(200),
-                    },
-                    {
-                        testId : BigInt(3),
-                        testVal : BigInt(300),
-                    },
-                ]
-            );
-        });
-
-    t.end();
-});
 };
--- unified-test/input/_from-ununified/table/update-zero-or-one-by-super-key/zero.ts
+++ unified-test/input/_from-ununified/table/update-zero-or-one-by-super-key/zero.ts
@@ -3,126 +3,127 @@ import {Test} from "../../../../test";
 import * as tsql from "../../../../../dist";
 
 export const test : Test = ({tape, pool, createTemporarySchema}) => {
-tape(__filename, async (t) => {
-    const dst = tsql.table("dst")
-        .addColumns({
-            testId : tm.mysql.bigIntUnsigned(),
-            testVal : tm.mysql.bigIntUnsigned(),
-        })
-        .setPrimaryKey(columns => [columns.testId])
-        .addMutable(columns => [
-            columns.testVal,
-        ]);
+    tape(__filename, async (t) => {
+        const dst = tsql.table("dst")
+            .addColumns({
+                testId : tm.mysql.bigIntUnsigned(),
+                testVal : tm.mysql.bigIntUnsigned(),
+            })
+            .setPrimaryKey(columns => [columns.testId])
+            .addMutable(columns => [
+                columns.testVal,
+            ]);
 
-    const result = await pool.acquire(async (connection) => {
-        await createTemporarySchema(
-            connection,
-            {
-                tables : [
-                    {
-                        tableAlias : "dst",
-                        columns : [
-                            {
-                                columnAlias : "testId",
-                                dataType : {
-                                    typeHint : tsql.TypeHint.BIGINT_SIGNED,
-                                },
-                            },
-                            {
-                                columnAlias : "testVal",
-                                dataType : {
-                                    typeHint : tsql.TypeHint.BIGINT_SIGNED,
-                                },
-                            },
-                        ],
-                        primaryKey : {
-                            multiColumn : false,
-                            columnAlias : "testId",
-                            autoIncrement : false,
-                        },
-                    }
-                ]
-            }
-        );
-
-        await dst
-            .enableExplicitAutoIncrementValue()
-            .insertMany(
+        const result = await pool.acquire(async (connection) => {
+            await createTemporarySchema(
                 connection,
-                [
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(2),
-                        testVal : BigInt(200),
-                    },
-                    {
-                        testId : BigInt(3),
-                        testVal : BigInt(300),
-                    },
-                ]
+                {
+                    tables : [
+                        {
+                            tableAlias : "dst",
+                            columns : [
+                                {
+                                    columnAlias : "testId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "testVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                            primaryKey : {
+                                multiColumn : false,
+                                columnAlias : "testId",
+                                autoIncrement : false,
+                            },
+                        }
+                    ]
+                }
             );
 
-        return dst.updateZeroOrOneBySuperKey(
-            connection,
-            {
-                testId : BigInt(1),
-                testVal : BigInt(99),
-            },
-            columns => {
-                return {
-                    testVal : tsql.integer.add(
-                        columns.testVal,
-                        BigInt(50)
-                    ),
-                };
-            }
-        );
-    });
-    t.deepEqual(
-        result.foundRowCount,
-        BigInt(0)
-    );
-    t.deepEqual(
-        result.updatedRowCount,
-        BigInt(0)
-    );
-    t.deepEqual(
-        result.warningCount,
-        BigInt(0)
-    );
+            await dst
+                .enableExplicitAutoIncrementValue()
+                .insertMany(
+                    connection,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                    ]
+                );
 
-    await pool
-        .acquire(async (connection) => {
-            return tsql.from(dst)
-                .select(columns => [columns])
-                .orderBy(columns => [
-                    columns.testId.asc(),
-                ])
-                .fetchAll(connection);
-        })
-        .then((rows) => {
-            t.deepEqual(
-                rows,
-                [
-                    {
-                        testId : BigInt(1),
-                        testVal : BigInt(100),
-                    },
-                    {
-                        testId : BigInt(2),
-                        testVal : BigInt(200),
-                    },
-                    {
-                        testId : BigInt(3),
-                        testVal : BigInt(300),
-                    },
-                ]
-            );
+            return dst
+                .whereEqSuperKey({
+                    testId : BigInt(1),
+                    testVal : BigInt(99),
+                })
+                .updateZeroOrOne(
+                    connection,
+                    columns => {
+                        return {
+                            testVal : tsql.integer.add(
+                                columns.testVal,
+                                BigInt(50)
+                            ),
+                        };
+                    }
+                );
         });
+        t.deepEqual(
+            result.foundRowCount,
+            BigInt(0)
+        );
+        t.deepEqual(
+            result.updatedRowCount,
+            BigInt(0)
+        );
+        t.deepEqual(
+            result.warningCount,
+            BigInt(0)
+        );
 
-    t.end();
-});
+        await pool
+            .acquire(async (connection) => {
+                return tsql.from(dst)
+                    .select(columns => [columns])
+                    .orderBy(columns => [
+                        columns.testId.asc(),
+                    ])
+                    .fetchAll(connection);
+            })
+            .then((rows) => {
+                t.deepEqual(
+                    rows,
+                    [
+                        {
+                            testId : BigInt(1),
+                            testVal : BigInt(100),
+                        },
+                        {
+                            testId : BigInt(2),
+                            testVal : BigInt(200),
+                        },
+                        {
+                            testId : BigInt(3),
+                            testVal : BigInt(300),
+                        },
+                    ]
+                );
+            });
+
+        t.end();
+    });
 };
