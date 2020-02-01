bb726b3878dc63dc2d86ae7b387cb6931e5161b2
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-01 04:32:49 -0500

    Experiment with better emit related to #176

--- src/query-base/util/helper-type/one-row.ts
+++ src/query-base/util/helper-type/one-row.ts
@@ -5,41 +5,55 @@ import {LimitClause} from "../../../limit-clause";
 import {MapDelegate} from "../../../map-delegate";
 import {GroupByClause} from "../../../group-by-clause";
 
+export interface OneRowBeforeFromClause extends IQueryBase<{
+    /**
+     * No `FROM` clause allowed.
+     */
+    fromClause : FromClauseUtil.BeforeFromClause,
+    selectClause : SelectClause|undefined,
+
+    limitClause : LimitClause|undefined,
+
+    /**
+     * No compound queries allowed
+     */
+    compoundQueryClause : undefined,
+    compoundQueryLimitClause : LimitClause|undefined,
+
+    mapDelegate : MapDelegate|undefined,
+    groupByClause : GroupByClause|undefined,
+}> {
+
+}
+
+export interface OneRowEmptyGroupingSet extends IQueryBase<{
+    fromClause : IFromClause,
+    selectClause : SelectClause|undefined,
+
+    limitClause : LimitClause|undefined,
+
+    /**
+     * No compound queries allowed
+     */
+    compoundQueryClause : undefined,
+    compoundQueryLimitClause : LimitClause|undefined,
+
+    mapDelegate : MapDelegate|undefined,
+    /**
+     * Empty grouping set
+     */
+    groupByClause : readonly never[],
+}> {
+
+}
+
 /**
  * To guarantee a query returns one row only,
  * you cannot have a `FROM` clause or `COMPOUND QUERY` clause.
  *
  * Either that, or you must have `GROUP BY ()` and no `COMPOUND QUERY` clause.
  */
-export type OneRow = (
-    | IQueryBase<{
-        fromClause : FromClauseUtil.BeforeFromClause,
-        selectClause : SelectClause|undefined,
-
-        limitClause : LimitClause|undefined,
-
-        compoundQueryClause : undefined,
-        compoundQueryLimitClause : LimitClause|undefined,
-
-        mapDelegate : MapDelegate|undefined,
-        groupByClause : GroupByClause|undefined,
-    }>
-    | IQueryBase<{
-        fromClause : IFromClause,
-        selectClause : SelectClause|undefined,
-
-        limitClause : LimitClause|undefined,
-
-        /**
-         * No compound queries allowed
-         */
-        compoundQueryClause : undefined,
-        compoundQueryLimitClause : LimitClause|undefined,
-
-        mapDelegate : MapDelegate|undefined,
-        /**
-         * Empty grouping set
-         */
-        groupByClause : readonly never[],
-    }>
-);
+export type OneRow =
+    | OneRowBeforeFromClause
+    | OneRowEmptyGroupingSet
+;
