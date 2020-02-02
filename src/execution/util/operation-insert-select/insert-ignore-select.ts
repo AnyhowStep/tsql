a454bf7330683a1a7edb464cd66047437b36563e
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-02 05:36:04 -0500

    onInsertSelect is now invoked by INSERT IGNORE ... SELECT; #54

--- src/execution/util/operation-insert-select/insert-ignore-select.ts
+++ src/execution/util/operation-insert-select/insert-ignore-select.ts
@@ -2,6 +2,7 @@ import {QueryBaseUtil} from "../../../query-base";
 import {InsertableTable, TableUtil} from "../../../table";
 import {InsertSelectDelegate, InsertSelectUtil} from "../../../insert-select";
 import {InsertIgnoreManyResult, InsertIgnoreSelectConnection} from "../../connection";
+import {InsertSelectEvent} from "../../../event";
 
 export async function insertIgnoreSelect<
     QueryT extends QueryBaseUtil.AfterSelectClause & QueryBaseUtil.NonCorrelated,
@@ -14,10 +15,25 @@ export async function insertIgnoreSelect<
 ) : Promise<InsertIgnoreManyResult> {
     TableUtil.assertInsertEnabled(table);
 
-    const row = InsertSelectUtil.insertSelect(query, table, rowDelegate);
-    return connection.insertIgnoreSelect(
-        query,
-        table,
-        row
-    );
+    return connection.lock(async (connection) : Promise<InsertIgnoreManyResult> => {
+        const insertSelectRow = InsertSelectUtil.insertSelect(query, table, rowDelegate);
+        const insertResult = await connection.insertIgnoreSelect(
+            query,
+            table,
+            insertSelectRow
+        );
+
+        const fullConnection = connection.tryGetFullConnection();
+        if (fullConnection != undefined) {
+            await fullConnection.eventEmitters.onInsertSelect.invoke(new InsertSelectEvent({
+                connection : fullConnection,
+                query,
+                table,
+                insertSelectRow,
+                insertResult,
+            }));
+        }
+
+        return insertResult;
+    });
 }
