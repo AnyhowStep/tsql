5e589ad40f70554166976e808c08012353ad768a
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-07 16:59:09 -0500

    Added integer.avg() tests

--- src/expr-library/integer/aggregate/avg.ts
+++ src/expr-library/integer/aggregate/avg.ts
@@ -9,14 +9,38 @@ import {decimalMapper} from "../../decimal/decimal-mapper";
 /**
  * The return type being `DECIMAL` is intentional.
  */
-const avgImpl = makeAggregateOperator2<OperatorType.AGGREGATE_AVERAGE, boolean, bigint, Decimal|null>(
+const avgImpl = makeAggregateOperator2<OperatorType.AGGREGATE_AVERAGE, boolean, bigint|null, Decimal|null>(
     OperatorType.AGGREGATE_AVERAGE,
     decimalMapper.orNull(),
     TypeHint.BIGINT_SIGNED
 );
 
-export const avgDistinct : AggregateOperator1<bigint, Decimal|null> = <
-    ArgT extends BuiltInExpr_NonAggregate<bigint>
+/**
+ * Returns the average value of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_avg
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#avg
+ *
+ * -----
+ *
+ * + MySQL      : `AVG(DISTINCT x)`
+ * + PostgreSQL : `AVG(DISTINCT x)`
+ * + SQLite     : `AVG(DISTINCT x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ * + MySQL tends to return fewer decimal places.
+ * + PostgreSQL tends to return more decimal places.
+ * + SQLite uses double precision arithmetic, rather than fixed precision.
+ *
+ * @todo Some kind of `DECIMAL` polyfill for SQLite.
+ */
+export const avgDistinct : AggregateOperator1<bigint|null, Decimal|null> = <
+    ArgT extends BuiltInExpr_NonAggregate<bigint|null>
 >(
     arg : ArgT
 ) : (
@@ -25,8 +49,32 @@ export const avgDistinct : AggregateOperator1<bigint, Decimal|null> = <
     return avgImpl(true, arg) as ExprUtil.AggregateIntersect<Decimal|null, ArgT>;
 };
 
-export const avgAll : AggregateOperator1<bigint, Decimal|null> = <
-    ArgT extends BuiltInExpr_NonAggregate<bigint>
+/**
+ * Returns the average value of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_avg
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#avg
+ *
+ * -----
+ *
+ * + MySQL      : `AVG(DISTINCT x)`
+ * + PostgreSQL : `AVG(DISTINCT x)`
+ * + SQLite     : `AVG(DISTINCT x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ * + MySQL tends to return fewer decimal places.
+ * + PostgreSQL tends to return more decimal places.
+ * + SQLite uses double precision arithmetic, rather than fixed precision.
+ *
+ * @todo Some kind of `DECIMAL` polyfill for SQLite.
+ */
+export const avgAll : AggregateOperator1<bigint|null, Decimal|null> = <
+    ArgT extends BuiltInExpr_NonAggregate<bigint|null>
 >(
     arg : ArgT
 ) : (
@@ -35,4 +83,28 @@ export const avgAll : AggregateOperator1<bigint, Decimal|null> = <
     return avgImpl(false, arg) as ExprUtil.AggregateIntersect<Decimal|null, ArgT>;
 };
 
+/**
+ * Returns the average value of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_avg
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#avg
+ *
+ * -----
+ *
+ * + MySQL      : `AVG(DISTINCT x)`
+ * + PostgreSQL : `AVG(DISTINCT x)`
+ * + SQLite     : `AVG(DISTINCT x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ * + MySQL tends to return fewer decimal places.
+ * + PostgreSQL tends to return more decimal places.
+ * + SQLite uses double precision arithmetic, rather than fixed precision.
+ *
+ * @todo Some kind of `DECIMAL` polyfill for SQLite.
+ */
 export const avg = avgAll;
