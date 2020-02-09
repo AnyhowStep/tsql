089ce6430fc9b1453ecfded98597ee3bbcbbb832
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-09 16:46:29 -0500

    Added JSDoc to ifNull()

--- src/expr-library/control-flow/if-null.ts
+++ src/expr-library/control-flow/if-null.ts
@@ -7,6 +7,30 @@ import {OperatorType} from "../../operator-type";
 import {TypeOfCoalesce} from "./type-of-coalesce";
 import {BaseType} from "../../type-util";
 
+/**
+ * Equivalent to `COALESCE()` with two arguments.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/control-flow-functions.html#function_ifnull
+ * + https://www.postgresql.org/docs/9.2/functions-conditional.html#FUNCTIONS-COALESCE-NVL-IFNULL
+ * + https://www.sqlite.org/lang_corefunc.html#ifnull
+ *
+ * -----
+ *
+ * `COALESCE()` is part of the SQL standard.
+ * `IFNULL()` is DB-specific.
+ *
+ * But `IFNULL()` should behave the same as `COALESCE(x, y)`.
+ *
+ * -----
+ *
+ * + MySQL        : `IFNULL(x, y)`
+ * + PostgreSQL   : `COALESCE(x, y)`
+ * + SQLite       : `IFNULL(x, y)`
+ *
+ * -----
+ *
+ * @see coalesce
+ */
 export function ifNull<
     Arg0T extends AnyBuiltInExpr,
     Arg1T extends BuiltInExpr<BaseType<BuiltInExprUtil.TypeOf<Arg0T>>|null>
