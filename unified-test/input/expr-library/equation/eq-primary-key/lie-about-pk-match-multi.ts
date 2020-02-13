a4d727ef50a2629a9e7b395f803f971546aa532b
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-13 12:07:57 -0500

    Added eqPrimaryKey() tests

---
+++ unified-test/input/expr-library/equation/eq-primary-key/lie-about-pk-match-multi.ts
@@ -0,0 +1,99 @@
+import * as tm from "type-mapping";
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool, createTemporarySchema}) => {
+    tape(__filename, async (t) => {
+        const myTable = tsql.table("myTable")
+            .addColumns({
+                myTableId : tm.mysql.bigIntSigned(),
+                myTableVal : tm.mysql.bigIntSigned(),
+            })
+            /**
+             * Declare this a primary key
+             */
+            .setPrimaryKey(columns => [columns.myTableId]);
+
+        await pool.acquire(async (connection) => {
+            await createTemporarySchema(
+                connection,
+                {
+                    tables : [
+                        /**
+                         * But we don't actually make it a primary key on the database
+                         */
+                        {
+                            tableAlias : "myTable",
+                            columns : [
+                                {
+                                    columnAlias : "myTableId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "myTableVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                        },
+                    ]
+                }
+            );
+
+            await myTable.insertMany(
+                connection,
+                [
+                    {
+                        myTableId : BigInt(2),
+                        myTableVal : BigInt(200),
+                    },
+                    {
+                        myTableId : BigInt(2),
+                        myTableVal : BigInt(200),
+                    },
+                    {
+                        myTableId : BigInt(3),
+                        myTableVal : BigInt(300),
+                    },
+                    {
+                        myTableId : BigInt(3),
+                        myTableVal : BigInt(300),
+                    },
+                    {
+                        myTableId : BigInt(3),
+                        myTableVal : BigInt(300),
+                    },
+                ]
+            );
+            for (let i=2; i<=3; ++i) {
+                await tsql.from(myTable)
+                    .where(() => tsql.eqPrimaryKey(
+                        myTable,
+                        {
+                            myTableId : BigInt(i),
+                        }
+                    ))
+                    .select(columns => [columns])
+                    .fetchAll(connection)
+                    .then((rows) => {
+                        t.deepEqual(rows.length, i);
+                        rows.forEach(row => t.deepEqual(
+                            row,
+                            {
+                                myTableId : BigInt(i),
+                                myTableVal : BigInt(i*100),
+                            }
+                        ));
+                    })
+                    .catch((err) => {
+                        t.fail(err.message);
+                    });
+            }
+        });
+
+        t.end();
+    });
+};
