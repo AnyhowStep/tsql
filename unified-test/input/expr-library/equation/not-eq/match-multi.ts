15ccec3f98342d6cd6e911265e74c80fc942103a
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-14 16:09:40 -0500

    Added notEq() tests

---
+++ unified-test/input/expr-library/equation/not-eq/match-multi.ts
@@ -0,0 +1,88 @@
+import * as tm from "type-mapping";
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool, createTemporarySchema}) => {
+    tape(__filename, async (t) => {
+        const myTable = tsql.table("myTable")
+            .addColumns({
+                myTableId : tm.mysql.bigIntSigned(),
+                myTableVal : tm.mysql.bigIntSigned(),
+            });
+
+        await pool.acquire(async (connection) => {
+            await createTemporarySchema(
+                connection,
+                {
+                    tables : [
+                        {
+                            tableAlias : "myTable",
+                            columns : [
+                                {
+                                    columnAlias : "myTableId",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                                {
+                                    columnAlias : "myTableVal",
+                                    dataType : {
+                                        typeHint : tsql.TypeHint.BIGINT_SIGNED,
+                                    },
+                                },
+                            ],
+                        },
+                    ]
+                }
+            );
+
+            await myTable.insertMany(
+                connection,
+                [
+                    {
+                        myTableId : BigInt(1),
+                        myTableVal : BigInt(100),
+                    },
+                    {
+                        myTableId : BigInt(2),
+                        myTableVal : BigInt(200),
+                    },
+                    {
+                        myTableId : BigInt(3),
+                        myTableVal : BigInt(300),
+                    },
+                ]
+            );
+            await tsql.from(myTable)
+                .where(columns => tsql.notEq(
+                    columns.myTableId,
+                    BigInt(1)
+                ))
+                .select(columns => [columns])
+                .orderBy(columns => [
+                    columns.myTableId.desc(),
+                ])
+                .fetchAll(connection)
+                .then((rows) => {
+                    t.deepEqual(
+                        rows,
+                        [
+                            {
+                                myTableId : BigInt(3),
+                                myTableVal : BigInt(300),
+                            },
+                            {
+                                myTableId : BigInt(2),
+                                myTableVal : BigInt(200),
+                            },
+                        ]
+                    );
+                })
+                .catch((err) => {
+                    t.fail(err.message);
+                });
+        });
+
+        t.end();
+    });
+};
