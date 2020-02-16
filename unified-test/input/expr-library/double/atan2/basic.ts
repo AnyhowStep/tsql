06627e0a828e048808a486b647aea83264c40fba
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-16 06:11:20 -0500

    Added atan2() tests

---
+++ unified-test/input/expr-library/double/atan2/basic.ts
@@ -0,0 +1,41 @@
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool}) => {
+    tape(__filename, async (t) => {
+        await pool.acquire(async (connection) => {
+            const arr = [
+                -30,
+                -20,
+                -10,
+                -1,
+                -0.5,
+                0,
+                0.5,
+                1,
+                10,
+                20,
+                30,
+            ];
+            for (const a of arr) {
+                for (const b of arr) {
+                    await tsql.selectValue(() => tsql.double.atan2(a, b))
+                        .fetchValue(connection)
+                        .then((value) => {
+                            const expected = Math.atan2(a, b);
+                            const margin = 0.000000000000001;
+                            t.true(
+                                Math.abs(value - expected) < margin,
+                                `ATAN2(${a}, ${b}) ~= ${Math.atan2(a, b)} ~/= ${value}`
+                            );
+                        })
+                        .catch((err) => {
+                            t.fail(err.message);
+                        });
+                }
+            }
+        });
+
+        t.end();
+    });
+};
