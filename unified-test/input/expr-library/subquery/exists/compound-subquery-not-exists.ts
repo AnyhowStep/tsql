249abd4fd8f70ee6c01cd4f918ba051d09c0f8bb
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-15 22:53:06 -0500

    Added exists() tests

---
+++ unified-test/input/expr-library/subquery/exists/compound-subquery-not-exists.ts
@@ -0,0 +1,27 @@
+import {Test} from "../../../../test";
+import * as tsql from "../../../../../dist";
+
+export const test : Test = ({tape, pool}) => {
+    tape(__filename, async (t) => {
+        await pool.acquire(async (connection) => {
+            await tsql
+                .selectValue(() => tsql.exists(
+                    tsql.selectValue(() => "hello")
+                        .unionDistinct(
+                            tsql.selectValue(() => "hello")
+                        )
+                        .compoundQueryLimit(0)
+                ))
+                .fetchValue(connection)
+                .then((value) => {
+                    t.deepEqual(value, false);
+                })
+                .catch((err) => {
+                    t.fail(err.message);
+                    t.fail(err.sql);
+                });
+        });
+
+        t.end();
+    });
+};
