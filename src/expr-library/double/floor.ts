c908f0fdf210c54199f96da49aa9b1bfeb132cdc
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-17 14:12:17 -0500

    Added FLOOR() tests

--- src/expr-library/double/floor.ts
+++ src/expr-library/double/floor.ts
@@ -4,6 +4,30 @@ import {TypeHint} from "../../type-hint";
 import {makeOperator1Idempotent} from "../factory";
 
 /**
+ * Returns the floor of the number
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/mathematical-functions.html#function_floor
+ * + https://www.postgresql.org/docs/9.1/functions-math.html#FUNCTIONS-MATH-FUNC-TABLE
+ * + https://stackoverflow.com/questions/7129249/getting-the-floor-value-of-a-number-in-sqlite
+ *
+ * -----
+ *
+ * + MySQL        : `FLOOR(x)`
+ * + PostgreSQL   : `FLOOR(x)`
+ * + SQLite         : None, implement with user-defined function
+ *
+ * -----
+ *
+ * ```sql
+ *  SELECT FLOOR(1.1)
+ *  > 1
+ *
+ *  SELECT FLOOR(-1.1)
+ *  > -2
+ * ```
+ *
+ * -----
+ *
  * This function is idempotent.
  * `FLOOR(FLOOR(x)) == FLOOR(x)`
  */
