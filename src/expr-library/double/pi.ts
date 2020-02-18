0968b6109ec8cefd6c199d44d131518c279dafb0
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-02-18 11:58:40 -0500

    Added PI() tests

--- src/expr-library/double/pi.ts
+++ src/expr-library/double/pi.ts
@@ -3,6 +3,25 @@ import {makeOperator0} from "../factory";
 import {OperatorType} from "../../operator-type";
 import {TypeHint} from "../../type-hint";
 
+/**
+ * Returns the value of pi
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/mathematical-functions.html#function_pi
+ * + https://www.postgresql.org/docs/9.1/functions-math.html#FUNCTIONS-MATH-FUNC-TABLE
+ * + https://en.wikipedia.org/wiki/Pi
+ *
+ * -----
+ *
+ * + MySQL          : `PI()`; MySQL's understanding of pi is... terrible
+ *   + https://github.com/AnyhowStep/tsql/issues/252
+ *   + The MySQL adapter library **should not** use `PI()`, it should use `3.141592653589793` instead
+ * + PostgreSQL     : `PI()` Returns `3.14159265358979`
+ * + SQLite         : None, implement using `3.141592653589793`
+ *
+ * -----
+ *
+ * In JS, `Math.PI` is `3.141592653589793`
+ */
 export const pi = makeOperator0<OperatorType.PI, number>(
     OperatorType.PI,
     tm.mysql.double(),
