2c102a1d036355c2140ea96d9326af604943a827
anyhowstep <5655961+AnyhowStep@users.noreply.github.com>
2020-03-04 02:02:42 -0500

    DECIMAL trigonometric functions removed because postgresql does not have them; added some simple double aggregate function tests

--- src/expr-library/decimal/fractional-div.ts
+++ src/expr-library/decimal/fractional-div.ts
@@ -5,7 +5,7 @@ import {Decimal} from "../../decimal";
 import {makeOperator2} from "../factory";
 
 /**
- * Performs regular floating-point division
+ * Performs regular fixed-point division
  */
 export const fractionalDiv = makeOperator2<OperatorType.FRACTIONAL_DIVISION, Decimal, Decimal>(
     OperatorType.FRACTIONAL_DIVISION,
--- src/expr-library/double/aggregate/sum.ts
+++ src/expr-library/double/aggregate/sum.ts
@@ -5,14 +5,33 @@ import {makeAggregateOperator2, AggregateOperator1} from "../../aggregate-factor
 import {BuiltInExpr_NonAggregate} from "../../../built-in-expr";
 import {ExprUtil} from "../../../expr";
 
-const sumImpl = makeAggregateOperator2<OperatorType.AGGREGATE_SUM, boolean, number, number|null>(
+const sumImpl = makeAggregateOperator2<OperatorType.AGGREGATE_SUM, boolean, number|null, number|null>(
     OperatorType.AGGREGATE_SUM,
     tm.mysql.double().orNull(),
     TypeHint.DOUBLE
 );
 
-export const sumDistinct : AggregateOperator1<number, number|null> = <
-    ArgT extends BuiltInExpr_NonAggregate<number>
+/**
+ * Returns the total sum of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_sum
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#sumunc
+ *
+ * -----
+ *
+ * + MySQL      : `SUM(DISTINCT x)`
+ * + PostgreSQL : `SUM(DISTINCT x)`
+ * + SQLite     : `SUM(DISTINCT x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ */
+export const sumDistinct : AggregateOperator1<number|null, number|null> = <
+    ArgT extends BuiltInExpr_NonAggregate<number|null>
 >(
     arg : ArgT
 ) : (
@@ -21,8 +40,27 @@ export const sumDistinct : AggregateOperator1<number, number|null> = <
     return sumImpl(true, arg) as ExprUtil.AggregateIntersect<number|null, ArgT>;
 };
 
-export const sumAll : AggregateOperator1<number, number|null> = <
-    ArgT extends BuiltInExpr_NonAggregate<number>
+/**
+ * Returns the total sum of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_sum
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#sumunc
+ *
+ * -----
+ *
+ * + MySQL      : `SUM(x)`
+ * + PostgreSQL : `SUM(x)`
+ * + SQLite     : `SUM(x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ */
+export const sumAll : AggregateOperator1<number|null, number|null> = <
+    ArgT extends BuiltInExpr_NonAggregate<number|null>
 >(
     arg : ArgT
 ) : (
@@ -31,4 +69,23 @@ export const sumAll : AggregateOperator1<number, number|null> = <
     return sumImpl(false, arg) as ExprUtil.AggregateIntersect<number|null, ArgT>;
 };
 
+/**
+ * Returns the total sum of non-`NULL` values from a group.
+ *
+ * It returns `NULL` if there are no non-`NULL` values.
+ *
+ * + https://dev.mysql.com/doc/refman/8.0/en/group-by-functions.html#function_sum
+ * + https://www.postgresql.org/docs/9.2/functions-aggregate.html#FUNCTIONS-AGGREGATE-TABLE
+ * + https://www.sqlite.org/lang_aggfunc.html#sumunc
+ *
+ * -----
+ *
+ * + MySQL      : `SUM(x)`
+ * + PostgreSQL : `SUM(x)`
+ * + SQLite     : `SUM(x)`
+ *
+ * -----
+ *
+ * No guarantees are made about the precision of the return type.
+ */
 export const sum = sumAll;
